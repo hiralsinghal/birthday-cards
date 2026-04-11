5e8c2c28d806a31662ed022b212eff399269ba4a
Hiral Singhal <103998329+hiralsinghal@users.noreply.github.com>
2026-04-11 22:29:54 +0530

    navbar done i believe this is my first time using react and tailwind css together properly so m slow but i will catch up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,15 @@ import { Link } from "react-router";
 
 export default function Navbar() {
     return (
-        <nav>
-            <Link to="/">Home</Link>
-            <Link to="/birthday-facts">Facts</Link>
-            <Link to="/countdown">Countdown</Link>
-            <Link to="/gift-ideas">Gift Ideas</Link>
+        <nav className="flex justify-between items-center bg-[#535C91] h-14">
+            <Link to="/" className="px-4 py-4 hover:bg-[#1b1a55]">Home</Link>
+
+            <ul className="flex list-none p-0 m-0">
+                <li><Link to="/birthday-facts" className="block px-4 py-4 hover:bg-[#1B1A55]">Facts</Link></li>
+                <li><Link to="/countdown" className="block px-4 py-4 hover:bg-[#1b1a55]">Countdown</Link></li>
+                <li><Link to="/gift-ideas" className="block px-4 py-4 hover:bg-[#1b1a55]">Gift Ideas</Link></li>
+            </ul>
+            
         </nav>
     )
 }
