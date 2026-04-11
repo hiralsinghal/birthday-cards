fe4e933fc811e78767bd847f3eb06c7bb74d4155
Hiral Singhal <103998329+hiralsinghal@users.noreply.github.com>
2026-04-11 22:38:57 +0530

    add style to home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,9 +2,9 @@
 export default function Home() {
     return(
         <>
-            <main>
-                <h1>Home</h1>
-                <p>A birthday-themed project!</p>
+            <main className="flex flex-col items-center justify-center text-center min-h-screen">
+                <h1 className="text-9xl mb-3">Birthday</h1>
+                <p className="text-4xl mt-3">A birthday-themed project!</p>
             </main>
         </>
     )
