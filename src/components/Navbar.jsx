bcb6d66adb85dcaf355a2413a5eebadb9f0c5400
Hiral Singhal <103998329+hiralsinghal@users.noreply.github.com>
2026-04-12 22:16:51 +0530

    update colors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router";
 
 export default function Navbar() {
     return (
-        <nav className="flex justify-between items-center bg-[#535C91] h-14">
-            <Link to="/" className="px-4 py-4 hover:bg-[#1b1a55]">Home</Link>
+        <nav className="flex justify-between items-center bg-[#3BEBD6] h-14">
+            <Link to="/" className="px-4 py-4 text-black hover:bg-[#88B6EB]">Home</Link>
 
             <ul className="flex list-none p-0 m-0">
-                <li><Link to="/birthday-facts" className="block px-4 py-4 hover:bg-[#1B1A55]">Facts</Link></li>
-                <li><Link to="/countdown" className="block px-4 py-4 hover:bg-[#1b1a55]">Countdown</Link></li>
-                <li><Link to="/gift-ideas" className="block px-4 py-4 hover:bg-[#1b1a55]">Gift Ideas</Link></li>
+                <li><Link to="/birthday-facts" className="block px-4 py-4 hover:bg-[#88B6EB] text-black">Facts</Link></li>
+                <li><Link to="/countdown" className="block px-4 py-4 text-black hover:bg-[#88B6EB]">Countdown</Link></li>
+                <li><Link to="/gift-ideas" className="block px-4 py-4 text-black hover:bg-[#88B6EB]">Gift Ideas</Link></li>
             </ul>
             
         </nav>
