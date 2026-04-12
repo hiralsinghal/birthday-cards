7f0c45e2f9308a28c9268b8154f58b2da7bfeb25
Hiral Singhal <103998329+hiralsinghal@users.noreply.github.com>
2026-04-12 22:37:51 +0530

    add input field, colors tryna make it so that the user doesnt have to input the year

--- src/pages/Facts.jsx
+++ src/pages/Facts.jsx
@@ -1,10 +1,17 @@
 
 export default function Facts() {
     return(
-        <>
+        <div className="flex flex-col place-content-center">
+            <header>
+                <h1 className="p-8 text-4xl text-center">Facts about your Birthday</h1>
+            </header>
+
             <main>
-                <h1 className="text-white">Facts about your Birthday</h1>
+                <div className="bg-[#3B55EB] rounded-4xl ml-auto mr-auto w-52 p-4 flex flex-row text-center content-center justify-center">
+                    <input id="date" type="date" placeholder="DD/MM"></input>
+                    <button id="fetch" className="pl-2">Fetch</button>
+                </div>
             </main>
-        </>
+        </div>
     )
 }
