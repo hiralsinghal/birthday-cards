011e4f34301c6bbd564417e8e6579f0318cd840c
Hiral Singhal <103998329+hiralsinghal@users.noreply.github.com>
2026-04-12 22:42:55 +0530

    Add theme to countdown Install confetti cause bday!!

--- src/pages/Countdown.jsx
+++ src/pages/Countdown.jsx
@@ -2,7 +2,16 @@
 export default function Countdown() {
     return (
         <>
-            <h1>Countdown</h1>
+            <header>
+                <h1 className="p-8 text-4xl text-center">Countdown</h1>
+            </header>
+
+            <main>
+                <div className="bg-[#3B55EB] rounded-4xl ml-auto mr-auto w-52 p-4 flex flex-row text-center">
+                    <input id="date" type="date"></input>
+                    <button id="show" className="pl-2">Show</button>
+                </div>
+            </main>
         </>
     )
 }
